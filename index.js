ea0a22d738fb7a1d2711d21d53bb0adcace4774b
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-11 20:10:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((driver) => driver.toLowerCase() === string.toLowerCase())
+}
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter((driver) => driver.startsWith(string))
+}
+const matchName = (drivers, string) => drivers.filter((driver) => driver.name === string)
